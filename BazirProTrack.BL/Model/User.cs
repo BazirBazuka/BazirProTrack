a6dd60dc5a98738a189dfcbcdf1d5f1d6bc4784c
Boris Baluev <baluevingormatika@gmail.com>
2022-05-29 13:22:51 +0300

    Development started/Created Space for business logic, models and interface

---
+++ BazirProTrack.BL/Model/User.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BazirProTrack.BL.Model
+{
+    public class User
+    {
+    }
+}
---
+++ BazirProTrack.CMD/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BazirProTrack.CMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
